2ac4658e098a227a7e7916c4a2dfbeb6beb6fa03
danielmalle2023 <danielmalle2023@gmail.com>
2023-08-28 02:42:50 -0700

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: o (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+		putchar(x + '0');
+	putchar('\n');
+	return (0);
+}
