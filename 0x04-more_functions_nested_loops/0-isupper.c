dae984858e43521776b7ad69b9907ec5a16e07b9
danielmalle2023 <danielmalle>
2024-04-29 08:17:09 -0700

    solved

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 #include <stdio.h>
 /**
  * _isupper - check if c is upper
@@ -8,7 +8,8 @@ nclude "main.h"
 */
 
 int _isupper(int c)
-{       if (c >= 65 && c <= 90)
+{
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
